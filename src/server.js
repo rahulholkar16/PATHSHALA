7df8795c8fc7aadae033c068f127e56ad6ee7c4b
Rahul Pal <usefulltech1819@gmail.com>
2025-09-03 23:17:32 +0530

    add dbconfig

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from './app.js';
+import { connectDB } from './config/db.js';
+
+await connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log("Server start at: " + process.env.PORT);
