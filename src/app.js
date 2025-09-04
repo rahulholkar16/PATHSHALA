1f8d30cffdd9774788107a832fb7dbf29aac5b5c
Rahul Pal <usefulltech1819@gmail.com>
2025-09-05 01:10:06 +0530

    Add health check route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
+import healthCheck from './routes/healthCheck.routes.js';
 
-dotenv.config(); // 
+dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// cors confifrations
+app.use(cors({ 
+    origin: process.env.CORS_ORIGIN?.split(","),
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+
+app.use("/api/v1/healthcheck", healthCheck);
 
 app.get('/', (req, res) => {
     res.json({
