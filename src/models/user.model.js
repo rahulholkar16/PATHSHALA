7cb106767ea9914323b2b94db78f6086e4605a07
Rahul Pal <usefulltech1819@gmail.com>
2025-09-04 20:36:59 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    "name": { type: String, required: true },
+    "email": { type: String, unique: true, required: true },
+    "password": { type: String, required: true },
+    "role": { type: String, enum: ['student', 'instructor', 'admin'], default: 'student', required: true},
+    "avatar": { type: String },
+    "bio": { type: String },
+    "enrolledCourse": [ { type: Schema.Types.ObjectId, ref: "Course" } ],
+    "teachingCourse": [{ type: Schema.Types.ObjectId, ref: "Course" } ],
+    "resetPasswordToken": { type: String },
+    "isVerified": { type: Boolean, default: false }
+}, {timestamps: true});
+
+export const UserModel = mongoose.model("User", user);
