f614c7d3e1073edd965f67a5092b53b543a7634c
Rahul Pal <usefulltech1819@gmail.com>
2025-09-05 12:17:12 +0530

    Add extra things in user Schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,13 +6,21 @@ const user = new Schema({
     "name": { type: String, required: true },
     "email": { type: String, unique: true, required: true },
     "password": { type: String, required: true },
-    "role": { type: String, enum: ['student', 'instructor', 'admin'], default: 'student', required: true},
+    "role": { 
+        type: String, 
+        enum: ['student', 'instructor', 'admin'], 
+        default: 'student', 
+        required: true
+    },
     "avatar": { type: String },
     "bio": { type: String },
     "enrolledCourse": [ { type: Schema.Types.ObjectId, ref: "Course" } ],
     "teachingCourse": [{ type: Schema.Types.ObjectId, ref: "Course" } ],
+    "isVerified": { type: Boolean, default: false },
+    "verificationToken": { type: String },
+    "verificationTokenExpire": { type: Date },
     "resetPasswordToken": { type: String },
-    "isVerified": { type: Boolean, default: false }
+    "resetPasswordExpire": { type: Date },
 }, {timestamps: true});
 
 export const UserModel = mongoose.model("User", user);
