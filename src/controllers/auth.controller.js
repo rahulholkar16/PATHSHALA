09619d9ee85d6b05fa212c27dbf90d75ba7c77d5
Rahul Pal <usefulltech1819@gmail.com>
2025-09-06 13:43:21 +0530

    Add logout route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,7 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
     await sendEmail({
         email: newUser.email,
         subject: "Plesase verify your email.",
-        mailgenContent: emailVerificationContent(newUser.name, `${req.protocal}://${req.get("host")}/api/v1/user/verify-email/${unHashedToken}`),
+        mailgenContent: emailVerificationContent(newUser.name, `${req.protocol}://${req.get("host")}/api/v1/user/verify-email/${unHashedToken}`),
     })
     
     const data = await UserModel.findById(newUser._id).select(
@@ -64,7 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-    if(!email && !password) throw new ApiError(400, "Email and Password is required."); 
+    if(!email || !password) throw new ApiError(400, "Email and Password is required."); 
 
     const user = await UserModel.findOne({ email });
 
@@ -101,4 +101,28 @@ const loginUser = asyncHandler(async (req, res) => {
         );
 })
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+    await UserModel.findByIdAndUpdate(
+        req.user._id, 
+        {
+            $set: {
+                refreshToken: "",
+            }
+        },
+        {
+            new: true,
+        },
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged out."));
+});
+
+export { registerUser, loginUser, logoutUser };
