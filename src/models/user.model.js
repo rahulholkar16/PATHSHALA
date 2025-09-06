7d248d91b8a6e7ebc829a78de2c4f336127495a7
Rahul Pal <usefulltech1819@gmail.com>
2025-09-06 12:21:23 +0530

    fix user model bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,36 +30,29 @@ const user = new Schema({
 user.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password, 5);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
-user.method.isPasswordCorrect = async function (password) {
+user.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-user.method.generateAccessToken = function () {
+user.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email
     }, process.env.ACCESS_JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
 }
 
-user.method.generateRefreshToken = function () {
+user.methods.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email
     }, process.env.REFRESH_JWT_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
 }
 
-user.method.generateRefreshToken = function () {
-    return jwt.sign({
-        _id: this._id,
-        email: this.email
-    }, process.env.REFRESH_JWT_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
-}
-
-user.method.generateTempToken = function () {
+user.methods.generateTempToken = function () {
     const unHashedToken = crypto.randomBytes(20).toString("hex");
     const hashedToken = crypto
             .createHash("sha256")
