35ec5e9ebd1ec098e5834b8eae0a968f5223490b
Rahul Pal <usefulltech1819@gmail.com>
2025-09-06 13:44:21 +0530

    Add logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/auth.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
 import { ValidationMiddleware } from '../middlewares/validationMiddleware.js';
+import { auth } from "../middlewares/authMiddleware.middlewares.js";
 
 const router = Router();
 router.route("/register").post(ValidationMiddleware, registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(auth, logoutUser);
 export default router;
