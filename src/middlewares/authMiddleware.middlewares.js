8e9fd707616285b98ddecc741cec110d82b0cc4b
Rahul Pal <usefulltech1819@gmail.com>
2025-09-06 13:42:44 +0530

    Add auth middleware

---
+++ src/middlewares/authMiddleware.middlewares.js
@@ -0,0 +1,25 @@
+import { UserModel } from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import jwt from "jsonwebtoken";
+
+export const auth = asyncHandler( async (req, res, next) => {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+    if(!token) throw new ApiError(401, "Unauthorized request");
+
+    try {
+        const decode = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
+
+        const user = await UserModel.findById(decode?._id).select(
+            "-password -verificationToken -resetPasswordToken -refreshToken"
+        )
+
+        if(!user) throw new ApiError(401, "Invalid access token");
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Invalid access token");
+    }
+});
