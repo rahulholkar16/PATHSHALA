db9875de1bb5dff79e84659da9667fa501b0add1
Rahul Pal <usefulltech1819@gmail.com>
2025-09-07 20:28:19 +0530

    add all routes in Route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
-import { ValidationMiddleware } from '../middlewares/validationMiddleware.js';
+import { registerUser, loginUser, logoutUser, verifyEmail, refreshAccessToken, forgotPassword, resetForgotPassword, getCurrentUser, changeCurrentPassword, resendEmailVerification } from "../controllers/auth.controller.js";
+import { ValidationMiddleware, passwordValidator } from '../middlewares/validationMiddleware.js';
 import { auth } from "../middlewares/authMiddleware.middlewares.js";
 
 const router = Router();
+
 router.route("/register").post(ValidationMiddleware, registerUser);
 router.route("/login").post(loginUser);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:resetToken").post(passwordValidator, resetForgotPassword);
+
+// protected route
 router.route("/logout").post(auth, logoutUser);
+router.route("/current-user").get(auth, getCurrentUser);
+router.route("/changed-password").post(passwordValidator, auth, changeCurrentPassword);
+router.route("/resend-email-verification").post(auth, resendEmailVerification);
 export default router;
