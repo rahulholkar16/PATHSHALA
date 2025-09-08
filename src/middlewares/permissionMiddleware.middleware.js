fad6a6bec5dd2222d1c11bcc7605eedf91954508
Rahul Pal <usefulltech1819@gmail.com>
2025-09-08 17:29:17 +0530

    add permission middlewares

---
+++ src/middlewares/permissionMiddleware.middleware.js
@@ -0,0 +1,22 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+
+export const allowPermission = asyncHandler((...requiredPermission) => {
+    return async (req, res, next) => {
+        if (!req.role && !req.permission) throw new ApiError(401, "Unauthorized: No user context");
+
+        if (req.user.permissions.includes("ALL")) {
+            return next(); // admin full access
+        }
+
+        const hasPermission = requiredPermission.every(p =>
+            req.user.permissions.includes(p)
+        );
+
+        if (!hasPermission) {
+            throw new ApiError(403, "Forbidden: You do not have required permissions");
+        }
+
+        next();
+    }
+})
