f60067e11fa80d977242ebb407c291f1de354b92
Rahul Pal <usefulltech1819@gmail.com>
2025-09-08 17:29:57 +0530

    add some req in auth middlewares

--- src/middlewares/authMiddleware.middlewares.js
+++ src/middlewares/authMiddleware.middlewares.js
@@ -2,6 +2,7 @@ import { UserModel } from "../models/user.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import jwt from "jsonwebtoken";
+import { permissions } from "../utils/permissions.js";
 
 export const auth = asyncHandler( async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
@@ -18,6 +19,8 @@ export const auth = asyncHandler( async (req, res, next) => {
         if(!user) throw new ApiError(401, "Invalid access token");
 
         req.user = user;
+        req.role = user.role;
+        req.permissions = permissions[user.role] || [];
         next();
     } catch (error) {
         throw new ApiError(401, "Invalid access token");
