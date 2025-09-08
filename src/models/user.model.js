cb429469ca4a0cb84acd9261d4be0212dd983fcd
Rahul Pal <usefulltech1819@gmail.com>
2025-09-08 20:05:26 +0530

    fix some typo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,25 +32,25 @@ user.pre("save", async function (next) {
 
     this.password = await bcrypt.hash(this.password, 10);
     next();
-})
+});
 
 user.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
-}
+};
 
 user.methods.generateAccessToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email
     }, process.env.ACCESS_JWT_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
-}
+};
 
 user.methods.generateRefreshToken = function () {
     return jwt.sign({
         _id: this._id,
         email: this.email
     }, process.env.REFRESH_JWT_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
-}
+};
 
 user.methods.generateTempToken = function () {
     const unHashedToken = crypto.randomBytes(20).toString("hex");
@@ -61,6 +61,6 @@ user.methods.generateTempToken = function () {
 
     const tokenExpiry = Date.now() + (10*60*1000) // 10 min
     return { unHashedToken, hashedToken, tokenExpiry };
-}
+};
 
 export const UserModel = mongoose.model("User", user);
