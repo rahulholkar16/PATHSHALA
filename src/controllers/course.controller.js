eb7a33ae473b81952af91b4006379a10063c0fd8
Rahul Pal <usefulltech1819@gmail.com>
2025-09-09 07:55:03 +0530

    Add get courses route

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -4,7 +4,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { CourseModel } from "../models/course.model.js";
 import { uploadOnCloud } from "../services/fileUploder.services.js";
 
-const createCourse = asyncHandler(async (req, res) => {
+export const createCourse = asyncHandler(async (req, res) => {
     const user = req.user;
     const {
         title,
@@ -53,4 +53,61 @@ const createCourse = asyncHandler(async (req, res) => {
     });
 
     res.status(200).json( new ApiResponse(200, newCourse, "course added successfully."));
+});
+
+export const getCourse = asyncHandler(async (req, res) => {
+    const {
+        search, //title
+        category,
+        level,
+        language,
+        minPrice,
+        maxPrice,
+        sortBy,
+        page = 1,
+        limit = 10,
+    } = req.query;
+    
+    const filters = { status: "Published" };
+
+    if (search) {
+        filters.$or = [
+            {title: { $regex: search, $options: "i" }},
+            {description: { $regex: search, $options: "i" }},
+        ];
+    }
+
+    if (category) filters.category = { $in: [category] };
+    if (level) filters.level = level;
+    if (language) filters.language = language;
+    if (minPrice || maxPrice) {
+        filters.price = {};
+        if (minPrice) filters.price.$gte = Number(minPrice);
+        if (maxPrice) filters.price.$lte = Number(minPrice);
+    };
+
+    let sort = { createdAt: -1 };
+    if (sortBy === "price_low_high") sort = { price: 1};
+    if (sortBy === "price_high_low") sort = { price: -1};
+    if (sortBy === "rating") sort = { averageRating: -1};
+
+    const course = await CourseModel.find(filters)
+        .populate("instructor", "name email avatar")
+        .skip((Number(page) - 1) * Number(limit))
+        .limit(Number(limit))
+        .sort(sort);
+
+    const totalCourses = await CourseModel.countDocuments(filters);
+
+    return res.status(200).json(
+        new ApiResponse(200, {
+            course,
+            pagination: {
+                total: totalCourses,
+                page: Number(page),
+                limit: Number(limit),
+                totalPages: Math.ceil(totalCourses / limit),
+            }
+        }, "Courses fetched successfully")
+    );
 });
