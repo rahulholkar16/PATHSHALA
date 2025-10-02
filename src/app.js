0e2c480c79898afdb513dce0d6aff99e900a1239
Rahul Pal <usefulltech1819@gmail.com>
2025-10-02 13:44:32 +0530

    Add cluster model

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -16,16 +16,22 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookiesParse());
-app.use("/offline-host", express.static(path.join(__dirname, "offlineP2P/host")));
-app.use("/offline-student", express.static(path.join(__dirname, "offlineP2P/student")));
+app.use(
+    "/offline-host",
+    express.static(path.join(__dirname, "offlineP2P/host"))
+);
+app.use(
+    "/offline-student",
+    express.static(path.join(__dirname, "offlineP2P/student"))
+);
 
 // cors confifrations
-app.use(cors({ 
-    origin: process.env.CORS_ORIGIN?.split(","),
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/v1/healthcheck", healthCheck);
 app.use("/api/v1/auth", authRouter);
