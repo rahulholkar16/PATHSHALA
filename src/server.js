f48ce8e318c49b196c99981128f56f51cc0eb936
Rahul Pal <usefulltech1819@gmail.com>
2025-10-02 13:45:12 +0530

    Fix format

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
-import app from './app.js';
-import { connectDB } from './config/db.js';
+import app from "./app.js";
+import { connectDB } from "./config/db.js";
+import { availableParallelism } from "os";
+import cluster from "cluster";
 
-connectDB()
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log("Server start at: " + process.env.PORT);
+const numCPUs = availableParallelism();
+
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    connectDB()
+        .then(() => {
+            app.listen(process.env.PORT, () => {
+                console.log("Server start at: " + process.env.PORT);
+            });
+        })
+        .catch((err) => {
+            console.error("MongoDB connection error: ", err);
+            process.exit(1);
         });
-    })
-    .catch((err) => {
-        console.error("MongoDB connection error: ", err);
-        process.exit(1);
-    })
-
+}
