a8340128e2e1196f7b04d0600e8081644c09ef6c
Rahul Pal <usefulltech1819@gmail.com>
2026-02-03 02:29:07 +0530

    Remove Offline middleware

--- src/app.js
+++ src/app.js
@@ -16,14 +16,6 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookiesParse());
-app.use(
-    "/offline-host",
-    express.static(path.join(__dirname, "offlineP2P/host"))
-);
-app.use(
-    "/offline-student",
-    express.static(path.join(__dirname, "offlineP2P/student"))
-);
 
 // cors confifrations
 app.use(
