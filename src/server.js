16bae1920b6bf1d9bf44144fabbfbe9d0724d124
Rahul Pal <usefulltech1819@gmail.com>
2026-02-03 02:45:16 +0530

    Add auto restart worker

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ if (cluster.isPrimary) {
     }
 
     cluster.on("exit", (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
+        console.log(`worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
     });
 } else {
     connectDB()
